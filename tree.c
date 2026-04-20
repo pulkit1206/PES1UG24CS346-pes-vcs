04fa40f1960e24698df44c0646c0b23ae07057d4
indiemonkey <devpulkit06@gmail.com>
2026-04-20 19:40:04 +0530

    feat(tree): finalize recursive tree serialization and storage

--- tree.c
+++ tree.c
@@ -170,7 +170,14 @@ static int write_tree_recursive(IndexEntry **entries, int count, int depth, Obje
         }
     }
 
-    (void)tree; (void)id_out;
+    void *data;
+    size_t len;
+    if (tree_serialize(&tree, &data, &len) != 0) return -1;
+    if (object_write(OBJ_TREE, data, len, id_out) != 0) {
+        free(data);
+        return -1;
+    }
+    free(data);
     return 0;
 }
 
